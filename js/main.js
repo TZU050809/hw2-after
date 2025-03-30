ddd7cffce139658919f2f2abe30af6c2ec943035
rita <D1225488@o365.fcu.edu.tw>
2025-03-30 22:06:51 +0800

    1

--- js/main.js
+++ js/main.js
@@ -4,48 +4,48 @@
     // Spinner
     var spinner = function () {
         setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
+            var spinnerElement = document.getElementById('spinner');
+            if (spinnerElement) {
+                spinnerElement.classList.remove('show');
             }
         }, 1);
     };
     spinner();
-    
-    
+
     // Initiate the wowjs
     new WOW().init();
 
-
     // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+    window.addEventListener('scroll', function () {
+        var navbar = document.querySelector('.sticky-top');
+        if (window.scrollY > 300) {
+            navbar.classList.add('shadow-sm');
+            navbar.style.top = '0px';
         } else {
-            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+            navbar.classList.remove('shadow-sm');
+            navbar.style.top = '-100px';
         }
-    });
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
+    }, { passive: true });
 
+    // Back to top button
+    window.addEventListener('scroll', function () {
+        var backToTop = document.querySelector('.back-to-top');
+        if (window.scrollY > 300) {
+            backToTop.style.display = 'block';
+        } else {
+            backToTop.style.display = 'none';
+        }
+    }, { passive: true });
+
+    document.querySelector('.back-to-top')?.addEventListener('click', function () {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
 
     // Facts counter
     $('[data-toggle="counter-up"]').counterUp({
         delay: 10,
         time: 2000
     });
-
 
     // Date and time picker
     $('.date').datetimepicker({
@@ -55,7 +55,6 @@
         format: 'LT'
     });
 
-
     // Header carousel
     $(".header-carousel").owlCarousel({
         autoplay: false,
@@ -63,13 +62,12 @@
         items: 1,
         dots: true,
         loop: true,
-        nav : true,
-        navText : [
+        nav: true,
+        navText: [
             '<i class="bi bi-chevron-left"></i>',
             '<i class="bi bi-chevron-right"></i>'
         ]
     });
-
 
     // Testimonials carousel
     $(".testimonial-carousel").owlCarousel({
@@ -78,21 +76,15 @@
         center: true,
         dots: false,
         loop: true,
-        nav : true,
-        navText : [
+        nav: true,
+        navText: [
             '<i class="bi bi-arrow-left"></i>',
             '<i class="bi bi-arrow-right"></i>'
         ],
         responsive: {
-            0:{
-                items:1
-            },
-            768:{
-                items:2
-            }
+            0: { items: 1 },
+            768: { items: 2 }
         }
     });
 
-    
 })(jQuery);
-
